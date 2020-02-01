6417eaeb9a0f8be2bb20068c996c7113e8326be3
dkihe <dnkkihe@gmail.com>
2020-01-31 21:53:43 -1000

    Added rotation to dog

---
+++ gj2020/Assets/Scripts/Dog_Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog_Controller : MonoBehaviour
+{
+
+    public float rot_speed;
+    void Start() {
+        
+    }
+
+    void Update() {
+        float dir = Input.GetAxis("Vertical");
+        aim(dir);
+    }
+
+    void aim(float v_dir) {
+        if (Input.GetButton("Vertical")) {
+            transform.Rotate(new Vector3(0, 0, v_dir) * rot_speed * Time.deltaTime);
+            Debug.Log("press");
+        }   
+    }
+}
